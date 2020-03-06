883fee8c526715c3e61868a6018749f92862fe43
Avni Goel <avnigoel@Avnis-MacBook-Pro.local>
2020-03-06 16:07:33 -0500

    ribbit

---
+++ ribbit.js
@@ -0,0 +1,26 @@
+$(".one").click(function(){
+  $(".one").css({"background-color": "limegreen"});
+});
+
+$(".two").click(function(){
+  $(".two").css({"background-color": "green"});
+});
+
+$(".three").click(function(){
+  $(".three").css({"background-color": "darkgreen"});
+});
+
+
+
+function toggleParagraphs1 () {
+	$("p").hide()
+}
+
+
+function toggleParagraphs2 () {
+	$("p").show()
+}
+
+function toggleFrog () {
+	$(".frog1").hide()
+}
