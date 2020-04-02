620c62a3af19eccee7dfe806052f6e090a918a24
Avni Goel <avnigoel@Avnis-MacBook-Pro.local>
2020-04-02 08:28:28 -0700

    three pictures

---
+++ extension/scripts/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('h1').text("Want a white shirt that you absolutely could not buy at Target for $5?")
+    $('span').text("$700000000")
+
+    document.body.style.backgroundColor='red'
+
+    $( "img, source, picture" ).attr({"data-srcset": "https://i.imgur.com/xduZmoB.jpg", "srcset": "https://i.imgur.com/xduZmoB.jpg", "src": "https://i.imgur.com/xduZmoB.jpg", "data-original-src": "https://i.imgur.com/xduZmoB.jpg"})
+
+});
+
+
+/*
+var arrayVariable = [ ];
+$("note").each(function (i) 
+{
+        arrayVariable[i]=$(this).attr("noteId");
+});
+*/
+
+// $( "a[title='Sweatee Tank Top: White']" )
